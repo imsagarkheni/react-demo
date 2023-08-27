2d2c406c40897285a6846c0451f63838cae151f8
Sagar Kheni <sagarkheni34@gmail.com>
2023-08-27 21:42:58 +0530

    redux flow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <Link to='/' class="nav-link">Home</Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link to='/about' class="nav-link">About</Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link to='/product' class="nav-link">Product</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+        <div className='navbar'>
+            
+                            <Link to='/' class="active">Home</Link>
+                        
+                            <Link to='/about' class="#">About</Link>
+                        
+                            <Link to='/product' class="#">Product</Link>
+                      
         </div>
     )
 }
